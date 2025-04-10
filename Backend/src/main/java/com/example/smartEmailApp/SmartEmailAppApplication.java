3b5f368d8a9cc4f6c338e466dc58ea2eea3a656f
Praneet <praneetvkhp@gmail.com>
2025-04-10 21:33:34 +0530

    Initialized the backend

---
+++ Backend/src/main/java/com/example/smartEmailApp/SmartEmailAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartEmailApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartEmailAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartEmailAppApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/example/smartEmailApp/SmartEmailAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartEmailApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartEmailAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
